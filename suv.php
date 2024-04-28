eff726944b9a5fc5535740c80b059ba9ff61256a
bhanuj10 <g.bhanuj2005@gmail.com>
2024-04-28 12:19:12 +0530

    files add

---
+++ suv.php
@@ -0,0 +1,25 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">SUV Rentals</h1>
+            <p class="text-center">Powerful, versatile, and spacious, SUVs to conquer every ride with comfort and style.</p>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/suv1.jpg" class="card-img-top" alt="SUV 1">
+                <div class="card-body">
+                    <h5 class="card-title">SUV Model 1</h5>
+                    <p class="card-text">Brief description of the SUV model.</p>
+                    <a href="#" class="btn btn-primary">Rent Now</a>
+                </div>
+            </div>
+        </div>
+        <!-- Add more SUV cards here -->
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
