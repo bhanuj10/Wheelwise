124931e5c93d149f3eb3abc831cbda2b04aa93b0
bhanuj10 <g.bhanuj2005@gmail.com>
2024-05-01 01:02:22 +0530

    navbar added

--- index.php
+++ index.php
@@ -7,24 +7,36 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/styles.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
     </head>
     <body>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">WheelWise</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
-                            <!-- Add other navigation links here -->
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <nav class="navbar navbar-light navhome">
+        <div class="container-fluid">
+            <span class="navbar-brand navcenter">Wheelwise</span>
+        </div>
+        </nav>
+        
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">Home</a>
+            <a class="nav-link disabled" href="#" style="visibility:hidden">Profile</a>
+            <a class="nav-link disabled" href="#" style="visibility:hidden">Settings</a>
+            <a class="nav-link disabled" href="#" style="visibility:hidden">Logout</a>
+
+            
+        </div>
+
+        <!-- Use any element to open the sidenav -->
+        <div onclick="openNav()" >
+            <svg style=" border: 2px solid black;border-radius: 5px;" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"></path>
+            </svg>
+        </div>
+
+        <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+        <div id="main">
+        
+        </div>
+        
     </body>
 </html>
