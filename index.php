29e667bb79b4b686493ff2c14a71293dfed5b83c
bhanuj10 <g.bhanuj2005@gmail.com>
2024-05-01 13:46:30 +0530

    sidebar completed

--- index.php
+++ index.php
@@ -9,29 +9,35 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="js/scripts.js"></script>
     </head>
-    <body>
-        <nav class="navbar navbar-light navhome">
-        <div class="container-fluid">
-            <span class="navbar-brand navcenter">Wheelwise</span>
-        </div>
-        </nav>
-        
-        <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="#">Home</a>
-            <a class="nav-link disabled" href="#" style="visibility:hidden">Profile</a>
-            <a class="nav-link disabled" href="#" style="visibility:hidden">Settings</a>
-            <a class="nav-link disabled" href="#" style="visibility:hidden">Logout</a>
 
-            
+    <body>
+        <div class="container-fluid bg-secondary">
+
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Home</a>
+                <a class="nav-link" href="#" >Profile</a>
+                <a class="nav-link" href="#" >Settings</a>
+                <a class="nav-link" href="#" >Logout</a>
+                <a class="nav-link helpnav" href="#" >Help?</a>
+            </div>
+
+            <nav class="navbar navbar-light " style="">
+
+                <span onclick="openNav()">
+                    <svg class="bg-white svg-icon" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"></path>
+                    </svg>
+                </span>
+
+                <span class="navbar-brand" >Wheelwise</span>
+
+            </nav>
+
         </div>
 
         <!-- Use any element to open the sidenav -->
-        <div onclick="openNav()" >
-            <svg style=" border: 2px solid black;border-radius: 5px;" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"></path>
-            </svg>
-        </div>
+        
 
         <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
         <div id="main">
--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,5 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
+
+  
